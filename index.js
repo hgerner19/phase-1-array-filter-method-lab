0fda21e1691db303073c7fcc5aabd0c1f0a30c2b
Holden Gerner <hgerner19@gmail.com>
2023-02-23 10:12:27 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here
+function findMatching(array,word){
+    let temp = [];
+    for(const user of array){
+        if(user === word || user === word.toLowerCase()){
+            temp.push(user);
+        }
+    }
+    return temp;
+}
+function fuzzyMatch(array,word){
+    let temp =[];
+    for(const user of array){
+        if(user.startsWith(word)){
+            temp.push(user);
+        }
+    }
+    return temp;
+}
+function matchName(array,word){
+    let temp = [];
+    for(const user of array){
+        if(user.name === word || user.hometown === word){
+            temp.push(user);
+        }
+    }
+    return temp;
+}
